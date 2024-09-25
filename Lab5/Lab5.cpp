edf7bd5a7b73dff8c88298628ea608212b81d17f
spiderman2871 <cnelson2871@gmail.com>
2024-09-25 13:03:47 -0400

    commit 1

--- Lab5/Lab5.cpp
+++ Lab5/Lab5.cpp
@@ -2,19 +2,34 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    vector<char> cypher({ 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' });
+    string text;
+
+    const int max_limit = 128;
+
+    cout << "Input text to cypher:";
+    getline(cin, text); 
+
+    if (text.length() > max_limit) {
+        cout << "ERROR! CHARACTER lIMIT SURPASSED";
+        return 1;
+    }
+    for (int i=0; i < text.length(); ++i) {
+        if (text[0] > 65 and text[0] < 90) {
+            int translate;
+            text[i] == translate;
+            translate == translate - 65;
+        }
+        else if (text[0] > 97 and text[0] < 122) {
+
+        }
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
